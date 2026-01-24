c97c0f8e156369f645c63bc0615469276076fc35
Toan Le <thanhtoanhcm1602@gmail.com>
2026-01-24 16:16:53 +0700

    Update style sidebar.

--- app/script/routes.js
+++ app/script/routes.js
@@ -9,6 +9,21 @@ angular.module("AngularJSWeb").config([
       .when("/home", {
         templateUrl: "app/pages/home.html",
       })
+      .when("/basic-info", {
+        templateUrl: "app/pages/basic-info.html",
+      })
+      .when("/trial-info", {
+        templateUrl: "app/pages/trial-info.html",
+      })
+      .when("/subscription", {
+        templateUrl: "app/pages/subscription.html",
+      })
+      .when("/merchant", {
+        templateUrl: "app/pages/merchant.html",
+      })
+      .when("/patients", {
+        templateUrl: "app/pages/patients.html",
+      })
       .when("/phones", {
         template: "<phone-list-component></phone-list-component>",
       })
